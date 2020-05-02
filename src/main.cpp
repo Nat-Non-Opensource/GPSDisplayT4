e8ba8cc4f7c7647700e2f5f262d5f95c5f193cdc
pawatsaengduan <saengduanpawat@gmail.com>
2020-05-02 16:46:57 +0700

    Display the full gps module

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
-// The first time you need to define the board model and version
+/* Import the libraries */
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <TinyGPS++.h>
+#include <TFT_eSPI.h>
+
+/* Define the Board model and Board version */
 
 // #define T4_V12
 #define T4_V13
@@ -20,28 +26,36 @@
 #error "Please select board version."
 #endif
 
-#include <TFT_eSPI.h>
-#include <WiFi.h>
-#include <Wire.h>
+/* Define GPS Module Pin & Baudrate */
+#define RXPin (35)
+#define TXPin (33)
 
+static const uint32_t GPSBaud = 9600;
+
+/* Define lbibraries */
+HardwareSerial hs(2);
+TinyGPSPlus gps;
 TFT_eSPI tft = TFT_eSPI();
+
+/* Initialize the function */
+static void smartDelay(unsigned long ms);
+static void printFloat(float val, bool valid, int len, int prec);
+static void printInt(unsigned long val, bool valid, int len);
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t);
+static void printStr(const char *str, int len);
+
 
 void setup()
 {
+  /* Setup the baudrate */
   Serial.begin(115200);
-  delay(1000);
 
-  Serial.printf("Current select %s version\n", BOARD_VRESION);
-  Serial.printf("TFT_MISO:%d\n", TFT_MISO);
-  Serial.printf("TFT_MOSI:%d\n", TFT_MOSI);
-  Serial.printf("TFT_SCLK:%d\n", TFT_SCLK);
-  Serial.printf("TFT_CS:%d\n", TFT_CS);
-  Serial.printf("TFT_DC:%d\n", TFT_DC);
-  Serial.printf("TFT_RST:%d\n", TFT_RST);
-  Serial.printf("TFT_BL:%d\n", TFT_BL);
+  /* Setup GPS Module */  
+  hs.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin, false);
 
+  /* Initialize the TFT Display */
   tft.init();
-  tft.setRotation(0);
+  tft.setRotation(1);
   tft.fillScreen(TFT_WHITE);
   tft.setTextSize(1);
   tft.setTextColor(TFT_BLACK);
@@ -55,7 +69,135 @@ void setup()
 
 void loop()
 {
+  /* Clear TFT Screen */
   tft.setTextColor(TFT_BLACK, TFT_DARKGREY);
-  tft.drawString("Hello, World", tft.width() / 2, tft.height() / 2);
   tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 0);
+  
+  /* Display GPS */
+  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+
+  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+  printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  printInt(gps.location.age(), gps.location.isValid(), 5);
+  printDateTime(gps.date, gps.time);
+  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
+  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
+  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "*** ", 6);
+
+  unsigned long distanceKmToLondon =
+      (unsigned long)TinyGPSPlus::distanceBetween(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT,
+          LONDON_LON) /
+      1000;
+  printInt(distanceKmToLondon, gps.location.isValid(), 9);
+
+  double courseToLondon =
+      TinyGPSPlus::courseTo(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT,
+          LONDON_LON);
+
+  printFloat(courseToLondon, gps.location.isValid(), 7, 2);
+
+  const char *cardinalToLondon = TinyGPSPlus::cardinal(courseToLondon);
+
+  printStr(gps.location.isValid() ? cardinalToLondon : "*** ", 6);
+
+  printInt(gps.charsProcessed(), true, 6);
+  printInt(gps.sentencesWithFix(), true, 10);
+  printInt(gps.failedChecksum(), true, 9);
+  Serial.println();
+
+  smartDelay(1000);
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+}
+
+/* Set the function */
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do
+  {
+    while (hs.available())
+      gps.encode(hs.read());
+  } while (millis() - start < ms);
+}
+
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      tft.print('*');
+    tft.print(' ');
+  }
+  else
+  {
+    tft.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i = flen; i < len; ++i)
+      tft.print(' ');
+  }
+  smartDelay(0);
+}
+
+static void printInt(unsigned long val, bool valid, int len)
+{
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i = strlen(sz); i < len; ++i)
+    sz[i] = ' ';
+  if (len > 0)
+    sz[len - 1] = ' ';
+  tft.print(sz);
+  smartDelay(0);
+}
+
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
+{
+  if (!d.isValid())
+  {
+    tft.print(F("********** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    tft.print(sz);
+  }
+
+  if (!t.isValid())
+  {
+    tft.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+    tft.print(sz);
+  }
+
+  printInt(d.age(), d.isValid(), 5);
+  smartDelay(0);
+}
+
+static void printStr(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i = 0; i < len; ++i)
+    tft.print(i < slen ? str[i] : ' ');
+  smartDelay(0);
 }
