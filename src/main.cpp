42808dff9610622935451693d2b351907ea89e10
pawatsaengduan <saengduanpawat@gmail.com>
2020-05-04 14:29:56 +0700

    ignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
-/* Import the libraries */
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <TinyGPS++.h>
 #include <TFT_eSPI.h>
-
-/* Define the Board model and Board version */
+#include <SPI.h>
+#include <SD.h>
 
 // #define T4_V12
 #define T4_V13
@@ -26,33 +25,37 @@
 #error "Please select board version."
 #endif
 
-/* Define GPS Module Pin & Baudrate */
+SPIClass sdSPI(VSPI);
+#define IP5306_ADDR 0X75
+#define IP5306_REG_SYS_CTL0 0x00
+
 #define RXPin (35)
 #define TXPin (33)
 
 static const uint32_t GPSBaud = 9600;
 
-/* Define libraries */
 HardwareSerial hs(2);
 TinyGPSPlus gps;
 TFT_eSPI tft = TFT_eSPI();
 
-/* Initialize the function */
 static void smartDelay(unsigned long ms);
 static void printFloat(float val, bool valid, int len, int prec);
 static void printInt(unsigned long val, bool valid, int len);
 static void printDate(TinyGPSDate &d);
 static void printTime(TinyGPSTime &t);
+String setFilename(TinyGPSDate &d);
+void writeRoot(fs::FS &fs);
+
+unsigned long last1 = 0;
+unsigned long last2 = 0;
+char filename[12];
 
 void setup()
 {
-  /* Setup the baudrate */
   Serial.begin(115200);
 
-  /* Setup GPS Module */
   hs.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin, false);
 
-  /* Initialize the TFT Display */
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
@@ -64,48 +67,85 @@ void setup()
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
   }
+
+  Serial.print("Initializing SD Card...   ");
+  if (SD_CS > 0)
+  {
+    sdSPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
+    if (!SD.begin(SD_CS, sdSPI))
+    {
+      Serial.println("Failed!");
+      tft.setCursor(0, 223);
+      tft.println("SD Card: Mount failed!");
+      while (1)
+        ;
+    }
+    else
+    {
+      Serial.println("Success!");
+      tft.setCursor(0, 223);
+      tft.println("SD Card: Mount Success!");
+    }
+  }
 }
 
 void loop()
 {
-  /* Clear TFT Screen */
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE);
-  tft.setCursor(0, 0);
+  unsigned long now1 = millis() + 1000;
+  unsigned long now2 = millis() + 1000;
 
-  /* Display GPS */
-  tft.print("Satellites: ");
-  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
-  tft.print("\nHDOP: ");
-  printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
-  tft.print("\nLatitude: ");
-  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-  tft.print("\nLongitude: ");
-  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
-  tft.print("\nFix (Age): ");
-  printInt(gps.location.age(), gps.location.isValid(), 5);
-  tft.print("\nDate: ");
-  printDate(gps.date);
-  tft.print("\nTime: ");
-  printTime(gps.time);
-  tft.print("\nDate (Age): ");
-  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
-  tft.print("\nChars: ");
-  printInt(gps.charsProcessed(), true, 6);
-  tft.print("\nSentences: ");
-  printInt(gps.sentencesWithFix(), true, 10);
-  tft.print("\nChecksum: ");
-  printInt(gps.failedChecksum(), true, 9);
-
-  smartDelay(1000);
-
-  if (millis() > 5000 && gps.charsProcessed() < 10)
+  if (now1 - last1 >= 1000)
   {
-    tft.print(F("No GPS data received: check wiring"));
+    last1 = now1;
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+    tft.setCursor(0, 223);
+
+    if (millis() > 5000 && gps.charsProcessed() < 10)
+    {
+      tft.print(F("No GPS data received: check wiring"));
+    }
+
+    tft.setCursor(0, 0);
+    tft.print("Satellites: ");
+    printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+    tft.print("\nHDOP: ");
+    printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
+    tft.print("\nLatitude: ");
+    printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+    tft.print("\nLongitude: ");
+    printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+    tft.print("\nFix (Age): ");
+    printInt(gps.location.age(), gps.location.isValid(), 5);
+    tft.print("\nDate: ");
+    printDate(gps.date);
+    tft.print("\nTime: ");
+    printTime(gps.time);
+    tft.print("\nDate (Age): ");
+    printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+    tft.print("\nAlt (m): ");
+    printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
+    tft.print("\nCourse Speed Card: ");
+    printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
+    tft.print("\nChars: ");
+    printInt(gps.charsProcessed(), true, 6);
+    tft.print("\nSentences: ");
+    printInt(gps.sentencesWithFix(), true, 10);
+    tft.print("\nChecksum: ");
+    printInt(gps.failedChecksum(), true, 9);
+
+    smartDelay(1000);
+  }
+
+  if (now2 - last2 >= 10000)
+  {
+    last2 = now2;
+
+    writeRoot(SD);
   }
 }
 
-/* Set the function */
 static void smartDelay(unsigned long ms)
 {
   unsigned long start = millis();
@@ -159,27 +199,91 @@ static void printDate(TinyGPSDate &d)
   else
   {
     char sz[32];
-    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    sprintf(sz, "%02d/%02d/%04d ", d.month(), d.day(), d.year());
     tft.print(sz);
   }
-
   printInt(d.age(), d.isValid(), 5);
   smartDelay(0);
 }
 
 static void printTime(TinyGPSTime &t)
 {
-  if (!t.isValid())
+  char sz[32];
+  if (t.isValid())
   {
-    tft.print(F("******** "));
-  }
-  else
-  {
-    char sz[32];
     sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    tft.print(sz);
   }
 
   printInt(t.age(), t.isValid(), 5);
   smartDelay(0);
+}
+
+String setFilename(TinyGPSDate &d)
+{
+  if (!d.isValid())
+  {
+    sprintf(filename, "/NULLFiles.csv");
+  }
+  else
+  {
+    sprintf(filename, "/%02d%02d%04d.csv", d.day(), d.month(), d.year());
+  }
+
+  return (String)filename;
+}
+
+void writeRoot(fs::FS &fs)
+{
+  File root = fs.open(setFilename(gps.date), FILE_APPEND);
+  Serial.print("SD Card Write...   ");
+  if (root)
+  {
+    tft.setCursor(0, 223);
+    tft.print("\nSD Card: Working!");
+    Serial.println("Success");
+
+    root.print(gps.satellites.isValid());
+    root.print(",");
+    root.print(gps.satellites.value());
+    root.print(",");
+    root.print(gps.hdop.hdop());
+    root.print(",");
+    root.print(gps.location.lat());
+    root.print(",");
+    root.print(gps.location.lng());
+    root.print(",");
+    root.print(gps.location.age());
+    root.print(",");
+    root.print(gps.date.month());
+    root.print("/");
+    root.print(gps.date.day());
+    root.print("/");
+    root.print(gps.date.year());
+    root.print(",");
+    root.print(gps.time.hour());
+    root.print(":");
+    root.print(gps.time.minute());
+    root.print(":");
+    root.print(gps.time.second());
+    root.print(",");
+    root.print(gps.altitude.meters());
+    root.print(",");
+    root.print(gps.course.deg());
+    root.print(",");
+    root.print(gps.speed.kmph());
+    root.print(",");
+    root.print(gps.charsProcessed());
+    root.print(",");
+    root.print(gps.sentencesWithFix());
+    root.print(",");
+    root.println(gps.failedChecksum());
+
+    root.close();
+  }
+  else
+  {
+    Serial.println("Failed");
+    tft.setCursor(0, 223);
+    tft.print("\nSD Card: Failed to write");
+  }
 }
